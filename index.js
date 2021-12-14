8267234e0a7db6b9cb8843c429e2dd490bf9a268
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2021-12-14 15:57:13 +0100

    .

--- index.js
+++ index.js
@@ -1,123 +1,98 @@
 const playwright = require('playwright');
 const fs = require('fs');
 const datos = require('./datos.json');
-const { createDeflate } = require('zlib');
-const { TIMEOUT } = require('dns');
-var newjson = []
+let juvenil_url = datos.juvenil_url + `jornada=${datos.juvenil_jornada}`
+let newjson = []
 let campo
-async function juvenil() {
+let matchesstring
+let localteam
+let polideportivo
+let position
+let endposition
+let endteamline
+let visitteam
+let local
+let matches
+let i = 0;
+
+async function getdata() {
+    //Create the browser and a newtab
     const browser = await playwright.chromium.launch({
         headless: true,
     });
     const page = await browser.newPage();
-
+    //We say to the program that discard the images
     await page.route('**/*', (route) => {
-        return route.request().resourceType() === 'image'
-            ? route.abort()
-            : route.continue()
+        return route.request().resourceType() === 'image' ?
+            route.abort() :
+            route.continue()
     });
-    //JUVENIL FEM
-    await page.goto(`https://www.rfebm.com/competiciones/competicion.php?seleccion=0&id_categoria=200011&id_competicion=202747&id_temp=2122&id_ambito=0&id_territorial=31&jornada=${datos.juvenil_jornada}`);
-    i = 0;
-    console.log("Pagina cargada 💻")
-    //Pull to data all p-t-20 from the td
-    const equipos = await page.$$eval('td.p-t-20', (tds) => {
+    //Go to the page
+    await page.goto(juvenil_url);
+    //Get all the matches of the day
+    matches = await page.$$eval('td.p-t-20', (tds) => {
         return tds.map((td) => td.textContent);
     });
-    console.log("Tabla Recuperada✅")
-    let equiposstring = JSON.stringify(equipos)
 
-    equiposstring = equiposstring.substring(1, equiposstring.length - 1);
-    if (equiposstring.search(`"G.D.M LA MERCED  - `) != -1) {
-        campo = 1;
+    matchesstring = JSON.stringify(matches)
+    matchesstring = matchesstring.substring(1, matchesstring.length - 1);
+    if (matchesstring.search(`"G.D.M LA MERCED  - `) != -1) {
+        local = 1;
         console.log("Juega local🏠")
     } else {
-        if (equiposstring.search(`- G.D.M LA MERCED "`) != -1) {
-            campo = 0;
+        if (matchesstring.search(`- G.D.M LA MERCED "`) != -1) {
+            local = 0;
             console.log("Juega visitante🚗")
         } else {
             console.log("Este equipo no juega esta semana ❌")
         }
     }
-    // console.warn(equiposstring)
 
 
-
-    //     //VISITANTES
-    //     let iniciolinea = 
-    // //LOCALES
-
-    if (campo == 1) {
-        let equipolocaljueganlocaljuvenil = `G.D.M LA MERCED`
-        let polideportivo = `MIGUEL INDURAIN`
-        let position = equiposstring.search(`"G.D.M LA MERCED`)
-        let lineendposition = equiposstring.search(`"ACTA NO DISPONIBLE",`)
-        juveniljuegalocal = equiposstring.substring(position)
+    if (local == 1) {
+        localteam = `G.D.M LA MERCED`
+        polideportivo = `MIGUEL INDURAIN`
+        position = matchesstring.search(`"G.D.M LA MERCED`)
+        endposition = matchesstring.search(`"ACTA NO DISPONIBLE",`)
+        endteamline = matchesstring.substring(position)
 
 
-        juveniljuegalocal = juveniljuegalocal.substring(0, lineendposition)
+        endteamline = endteamline.substring(0, endposition)
         // console.log(juveniljuegalocal)
 
-        let endposition = juveniljuegalocal.search(`","`)
-        let juveniljuegalocalequipovisitante = juveniljuegalocal.substring(20, endposition)
+        endposition = endteamline.search(`","`)
+        visitteam = endteamline.substring(20, endposition)
         // console.log(juveniljuegalocalequipovisitante) //GURPEA BETI-ONAK B
         newjson.push(`${datos.juvenil_jornada}`)
         newjson.push(`${polideportivo}`)
-        newjson.push(`${equipolocaljueganlocaljuvenil}`)
-        newjson.push(`${juveniljuegalocalequipovisitante}`)
+        newjson.push(`${localteam}`)
+        newjson.push(`${visitteam}`)
 
     }
-    if (campo == 0) {
-        let equipovisitantejueganvisitantejuvenil = `G.D.M LA MERCED`
-        //OBTENER NOMBRE EQUIPO LOCAL
-        let position = equiposstring.search(`- G.D.M LA MERCED "`)
-        let equiposstringtrim = equiposstring.substring(0, position)
-        let lineendposition = equiposstringtrim.lastIndexOf(`ACTA NO DISPONIBLE","`)
-        let finposition = 21 + lineendposition
-        // console.log(finposition)
-        equipolocaljueganvisitantejuvenil = equiposstring.substring(finposition, position)
-        // console.log(equipolocaljueganvisitantejuvenil)
-        //Buscar campo del partido
-        lineaequipovisitantetemp = equiposstring.substring(finposition, equiposstring.length)
-        endlineaequipovistantetemp = (lineaequipovisitantetemp.search(`"ACTA NO DISPONIBLE","`)) - 80
-        lineaequipovisitante = lineaequipovisitantetemp.substring(0, endlineaequipovistantetemp)
-        trimpolideportivo = lineaequipovisitante.lastIndexOf(`"," - ","`) + 9
-        // console.log(trimpolideportivo)
-        polideportivo = lineaequipovisitante.substring(trimpolideportivo, lineaequipovisitante.length)
-        // console.log(polideportivo)
+    if (local == 0) {
+        visitteam = `G.D.M LA MERCED`
+        endposition = matchesstring.search(`- G.D.M LA MERCED "`)
+        equiposstringtrim = matchesstring.substring(0, endposition)
+        position = equiposstringtrim.lastIndexOf(`ACTA NO DISPONIBLE","`)
+        position = position + 21
+        localteam = matchesstring.substring(position, endposition)
+        // Get sportcenter
+        position = matchesstring.search(localteam)
+        
         newjson.push(`${datos.juvenil_jornada}`)
         newjson.push(`${polideportivo}`)
-        newjson.push(`${equipolocaljueganvisitantejuvenil}`)
-        newjson.push(`${equipovisitantejueganvisitantejuvenil}`)
-        
+        newjson.push(`${localteam}`)
+        newjson.push(`${visitteam}`)
+
     }
 
     browser.close();
+    console.log("Juvenil")
     console.log("Jornada: " + newjson[0] + " 🏁")
     console.log("Campo: " + newjson[1] + " 🏟️")
     console.log("Local: " + newjson[2] + " 🏃")
     console.log("Visitante: " + newjson[3] + " 🏃🏽‍♂️")
-    console.info("Cadete 🏃")
-
-}
-async function cadete() {
-    const browser = await playwright.chromium.launch({
-        headless: false,
-    });
-    const page = await browser.newPage();
-
-    await page.route('**/*', (route) => {
-        return route.request().resourceType() === 'image'
-            ? route.abort()
-            : route.continue()
-    });
-    //JUVENIL FEM
-    await page.goto(`https://www.rfebm.com/competiciones/competicion.php?seleccion=0&id=1008603&id_ambito=0jornada=${datos.cadete_jornada}`);
-    i = 0;
-    browser.close();
 
 
 }
-console.warn("JUVENIL 🏃🏻‍♂️")
-juvenil();
-cadete();
+getdata()
